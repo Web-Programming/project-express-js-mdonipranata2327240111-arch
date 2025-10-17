e92c821134d506cb6cff99e69476310c16e00779
mdonipranata2327240111-arch <mdonipranata_2327240111@mhs.mdp.ac.id>
2025-10-17 17:05:15 +0700

    Donipaw210-2025

--- toko-online/app.js
+++ toko-online/app.js
@@ -10,7 +10,8 @@ var usersRouter = require('./app_toko_online/routes/users');
 var productRouter = require("./app_toko_online/routes/product"); //letakkan di atas agar rapi
 var engine = require('ejs-blocks'); //menggunakan ejs block
 var app = express();
-
+var apiProductRouter = require("./app_toko_online/routes/api/product"); //import route ap
+var apiUserRouter = require('./app_toko_online/routes/api/users')
 // view engine setup
 app.set('views', path.join(__dirname, 'app_toko_online', 'views')); //perbaikan 1
 app.engine('ejs', engine);  //daftarkan engine ejs block
@@ -28,6 +29,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/produk", productRouter);
 
+app.use("/api/produk", apiProductRouter); //daftarkan route api
+app.use('/api/user', apiUserRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
